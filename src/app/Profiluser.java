058c4bd8bf8e9fac595faf4f62a40ce71cbf9735
asep1122a <septiyanadam1@gmail.com>
2025-01-11 18:43:40 +0700

    frist commit

---
+++ src/app/Profiluser.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author miqda
+ */
+public class Profiluser {
+    private int id_akun;
+
+    public int getId_profil() {
+        return id_profil;
+    }
+
+    public void setId_profil(int id_profil) {
+        this.id_profil = id_profil;
+    }
+    private int id_profil;
+    private String nama;
+    private String username;
+    private String password;
+    private String  role;
+
+    public int getId_akun() {
+        return id_akun;
+    }
+
+    public void setId_akun(int id_akun) {
+        this.id_akun = id_akun;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+
+    public String getTanggal_lahir() {
+        return tanggal_lahir;
+    }
+
+    public void setTanggal_lahir(String tanggal_lahir) {
+        this.tanggal_lahir = tanggal_lahir;
+    }
+    
+    private String alamat;
+    private String telepon;
+    private String tanggal_lahir;
+    
+
+}
